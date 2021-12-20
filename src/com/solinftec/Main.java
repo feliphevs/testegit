53ae328a0ef2f77cd5103751526da47ec307f419
Feliphe Silva <feliphevs90@outlook.com>
2021-12-20 17:39:26 -0300

    Teste

--- src/com/solinftec/Main.java
+++ src/com/solinftec/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println(1000);
-        System.out.println(2000);
+        System.out.println();
+        //kkldldodo
     }
 }
